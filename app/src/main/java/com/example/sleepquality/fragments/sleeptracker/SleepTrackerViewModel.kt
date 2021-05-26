e8c596a6a40285b0433b9c28767f6ddc50fe8539
Priscila Nitta <priscila.nitta@venturus.org.br>
2021-05-26 18:08:25 -0300

    ViewModel and fragment settings

--- app/src/main/java/com/example/sleepquality/fragments/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/sleepquality/fragments/sleeptracker/SleepTrackerViewModel.kt
@@ -1,4 +1,10 @@
 package com.example.sleepquality.fragments.sleeptracker
 
-class SleepTrackerViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.sleepquality.database.SleepDatabaseDao
+
+class SleepTrackerViewModel (val database: SleepDatabaseDao,
+    application: Application): AndroidViewModel(application) {
+
 }
--- app/src/main/java/com/example/sleepquality/fragments/sleeptracker/SleepTrackerViewModelFactory.kt
+++ app/src/main/java/com/example/sleepquality/fragments/sleeptracker/SleepTrackerViewModelFactory.kt
@@ -1,4 +1,20 @@
 package com.example.sleepquality.fragments.sleeptracker
 
-class SleepTrackerViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.sleepquality.database.SleepDatabaseDao
+
+class SleepTrackerViewModelFactory(
+    private val dataSource: SleepDatabaseDao,
+    private val application: Application
+) : ViewModelProvider.Factory {
+
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SleepTrackerViewModel::class.java)) {
+            return SleepTrackerViewModel(dataSource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
